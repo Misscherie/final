6f3e2efa640cbc57d8a3f457649aba29ac461a95
Ella.koles <ella.koles@yandex.ru>
2022-09-20 21:02:14 +0200

    program added

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿string[] array1 = new string[5] {"333", "berlin", "123", "578", "atlanta"};
+string[] result = new string[array1.Length];
+void ResultArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ResultArray(array1, result);
+PrintArray(result);
